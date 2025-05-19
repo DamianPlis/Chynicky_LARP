8a9a6aa858f640ab3a7b5478f6dd5766447e64e2
DamianPlis <damian.plis.praha@gmail.com>
2025-05-19 14:58:34 +0200

    yacatek generace kalendare

---
+++ script-generace-kalendare/kalendar-data.js
@@ -0,0 +1,20 @@
+export const data = [
+    {
+        nazev: "Název mocného",
+        datum:"14.6.-15.6.2025",
+        organizatori:[
+            "Kvido Redl",
+            "Hugo Redl",
+            "Kristián Páca",
+        ],
+        obrazek:"https://burthgulash.github.io/Chynicky_LARP/larphlavni/img/Výstřížek.PNG"
+    },
+    {
+        nazev:"Již brzy",
+        datum:"Již brzy",
+        organizatori:[
+            "Již brzy"
+        ],
+        obrazek:"https://burthgulash.github.io/Chynicky_LARP/larphlavni/img/questionmark2.2.png",
+    }
+]
