ac51ee0b620cab9d1303a856c20aea7cf6cba997
DamianPlis <damian.plis.praha@gmail.com>
2025-06-22 21:26:19 +0200

    funkci foto galerie s filtrem a directory change

---
+++ scripts/script-foto-galerie/foto-galerie-data.js
@@ -0,0 +1,26 @@
+export const fotoGalerieData = [
+    {
+        src: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/foto/Odehrane%20LARPy/OS2-Zpetne.foto/OS2-1.jpg",
+        akce: "OS2",
+    },
+    {
+        src: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/foto/Odehrane%20LARPy/OS2-Zpetne.foto/OS2-2.jpg",
+        akce: "OS2",
+    },
+    {
+        src: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/foto/Odehrane%20LARPy/OS2-Zpetne.foto/OS2-3.jpg",
+        akce: "OS2",
+    },
+    {
+        src: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/foto/Odehrane%20LARPy/OS2-Zpetne.foto/OS2-4.jpg",
+        akce: "OS1",
+    },
+    {
+        src: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/foto/Odehrane%20LARPy/OS2-Zpetne.foto/OS2-5.jpg",
+        akce: "OS1",
+    },
+    {
+        src: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/foto/Odehrane%20LARPy/OS2-Zpetne.foto/OS2-6.jpg",
+        akce: "OS1",
+    },
+]
