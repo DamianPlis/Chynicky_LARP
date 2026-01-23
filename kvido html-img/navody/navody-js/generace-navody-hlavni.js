96586348f6d167dde4fb337449d2cdbf4149f6e2
Burthgulash <kvido.redl@gmail.com>
2026-01-23 19:06:04 +0100

    ztvoření složky navody a obsahu (ještě nefunkční

---
+++ kvido html-img/navody/navody-js/generace-navody-hlavni.js
@@ -0,0 +1,58 @@
+import { data } from "./navody-hlavni-data.js";
+
+let htmlRendered = ""
+
+data.forEach((data, index) => {
+
+    /*let datumLongerThan1;
+    // zjisti na array jestli je 1 dlouhy nebo vic
+    if (data.datum.lenght < 1 ) {
+        datumLongerThan1 = true
+    }*/
+    let htmlRenderedOrganizatori = "";
+    let jizBrzycode = ""
+    data.organizatori.forEach(organizator => {
+        if (organizator === "Již brzy") {
+            htmlRenderedOrganizatori += `
+            <p class="organizatori-text">${organizator}</p>`
+            jizBrzycode = `id=icon-img data-name="questionmark2.2"`
+            return
+        }
+        htmlRenderedOrganizatori += `
+    <button class="organizatori-text" popovertarget="${organizator}">${organizator}</button>
+    `
+    });
+
+    let htmlRenderedDatum = ""
+    data.datum.forEach(datum => {
+        htmlRenderedDatum += `
+    <p class="datum-left">${datum}</p>
+    `
+    });
+    htmlRendered += `
+<div class="akce-container">
+    <div class="ozveny2-popis">
+        <a href="${data.link}">
+            <button id="akce-tlacitko" class="akce-tlacitko">${data.nazev}</button>
+        </a>
+        <div class="datumy">
+            <div style="margin: 0 0 10px 0;">
+                <p class="datum-right">Datum :</p>
+            </div>
+            <div class="moznost">
+                ${htmlRenderedDatum}
+            </div>
+        </div>
+        <div class="organizatori">
+            <p class="organizatori-text">Organizátoři :</p>
+            ${htmlRenderedOrganizatori}
+        </div>
+    </div>
+    <div class="ozveny2-img">
+        <img style="width: 100%; justify-content: center; margin: 5px 0 5px 0;" src="${data.obrazek}" ${jizBrzycode}>
+    </div>
+</div>
+`// TODO: udelat qestion mark light mod
+
+    document.querySelector(".kalendar-akci2").innerHTML = htmlRendered
+});
---
+++ kvido html-img/navody/navody-js/navody-hlavni-data.js
@@ -0,0 +1,28 @@
+export const data = [
+
+    {
+        nazev: "Návod na sekerovité zbraně",
+        datum: [
+            "5.6.-",
+            "7.6 2026",
+        ],
+        organizatori: [
+            "Kvido Redl",
+            "Hugo Redl",
+            "Kristian Páca",
+        ],
+        obrazek: "https://burthgulash.github.io/Chynicky_LARP/kvido%20html-img/Z.Popelu.kalicha-foto/Prapor-chatgpt.png",
+        link: "https://burthgulash.github.io/Chynicky_LARP/Z.Popelu.kalicha/Z.Popelu.kalicha.html"
+    },
+    {
+        nazev: "Již brzy",
+        datum: [
+            "Již brzy",
+        ],
+        organizatori: [
+            "Již brzy",
+        ],
+        obrazek: "https://burthgulash.github.io/kvido html-img/foto/Ikony-img/questionmark2.2.png",
+        link: "#"
+    },
+]
